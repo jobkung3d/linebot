403b1abd118d26a49de530c8cf94d09b28019145
jobkung3d <crystan_job.a@hotmail.com>
2019-07-09 00:49:17 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// Reply using AIML, parsing data with AIMLParser
+// Reply using AIML
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLParser = require('aimlparser')
+const AIMLInterpreter = require('aimlinterpreter')
 
 const app = express()
 const port = process.env.PORT || 4000
-const aimlParser = new AIMLParser({ name:'HelloBot' })
+const aimlInterpreter = new AIMLInterpreter({ name:'HelloBot'})
 
-aimlParser.load(['./test-aiml.xml'])
+aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+    aimlInterpreter.findAnswerInLoadedAIMLFiles(msg, (answer, wildCardArray, input) => {
         reply(reply_token, answer)
     })
     res.sendStatus(200)
@@ -35,7 +35,7 @@ function reply(reply_token, msg) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'test'
+            text: msg
         }]
     })
 
@@ -47,3 +47,4 @@ function reply(reply_token, msg) {
         console.log('status = ' + res.statusCode);
     });
 }
+
